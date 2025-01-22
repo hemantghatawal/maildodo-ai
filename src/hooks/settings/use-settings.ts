3aa43b91f246710f4920b8fa7ca8aab2be2f4d16
Hemant Ghatawal <ghatawal35@gmail.com>
2025-01-22 10:55:06 +0530

    domain settings page, code snippet to embed chatbot into websites and bot customization options

--- src/hooks/settings/use-settings.ts
+++ src/hooks/settings/use-settings.ts
@@ -1,23 +1,37 @@
 "use client";
-import { ChangePasswordProps, ChangePasswordSchema } from "@/schemas/auth.schema";
+import {
+  ChangePasswordProps,
+  ChangePasswordSchema,
+} from "@/schemas/auth.schema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { UploadClient } from "@uploadcare/upload-client";
 import { useTheme } from "next-themes";
 import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { useForm, useFormState } from "react-hook-form";
 import { useToast } from "../use-toast";
-import { onUpdatePassword } from "@/actions/settings";
+import {
+  onChatBotImageUpdate,
+  onDeleteUserDomain,
+  onUpdateDomain,
+  onUpdatePassword,
+  onUpdateWelcomeMessage,
+} from "@/actions/settings";
+import {
+  DomainSettingsProps,
+  DomainSettingsSchema,
+} from "@/schemas/settings.schema";
+import { useRouter } from "next/navigation";
 
-// const upload = new UploadClient({
-//   publicKey: process.env.NEXT_PUBLIC_UPLOAD_CARE_PUBLIC_KEY as string,
-// });
+const upload = new UploadClient({
+  publicKey: process.env.NEXT_PUBLIC_UPLOAD_CARE_PUBLIC_KEY as string,
+});
 
 export const useThemeMode = () => {
-  const {setTheme, theme} = useTheme()
+  const { setTheme, theme } = useTheme();
   return {
     setTheme,
-    theme
-  }
+    theme,
+  };
 };
 
 export const useChangePassword = () => {
@@ -51,5 +65,83 @@ export const useChangePassword = () => {
     errors,
     onChangePassword,
     loading,
+  };
+};
+
+export const useSettings = (id: string) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<DomainSettingsProps>({
+    resolver: zodResolver(DomainSettingsSchema),
+  });
+
+  const router = useRouter();
+  const { toast } = useToast();
+
+  const [loading, setLoading] = useState<boolean>(false);
+  const [deleting, setDeleting] = useState<boolean>(false);
+
+  const onUpdateSettings = handleSubmit(async (values: DomainSettingsProps) => {
+    setLoading(true);
+    if (values.domain) {
+      const domain = await onUpdateDomain(id, values.domain);
+      if (domain) {
+        toast({
+          title: "Success",
+          description: domain.message,
+        });
+      }
+    }
+
+    if (values.image[0]) {
+      const uploaded = await upload.uploadFile(values.image[0]);
+      const image = await onChatBotImageUpdate(id, uploaded.uuid);
+      if (image) {
+        toast({
+          title: image.status == 200 ? "Success" : "Error",
+          description: image.message,
+        });
+        setLoading(false);
+      }
+    }
+
+    if (values.welcomeMessage) {
+      const message = await onUpdateWelcomeMessage(id, values.welcomeMessage);
+      if (message) {
+        toast({
+          title: "Success",
+          description: message.message,
+        });
+      }
+    }
+
+    reset();
+    router.refresh();
+    setLoading(false);
+  });
+
+  const onDeleteDomain = async () => {
+    setDeleting(true);
+    const deleted = await onDeleteUserDomain(id);
+    if (deleted) {
+      toast({
+        title: "Success",
+        description: deleted.message,
+      });
+
+      setDeleting(false);
+      router.refresh();
+    }
+  };
+  return {
+    register,
+    onUpdateSettings,
+    errors,
+    loading,
+    onDeleteDomain,
+    deleting,
   };
 };
--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -63,11 +63,7 @@ export const ChangePasswordSchema: ZodType<ChangePasswordProps> = z
       .min(8, { message: "Your password must be atleast 8 characters long" })
       .max(64, {
         message: "Your password can not be longer then 64 characters long",
-      })
-      .refine(
-        (value) => /^[a-zA-Z0-9_.-]*$/.test(value ?? ""),
-        "password should contain only alphabets and numbers"
-      ),
+      }),
     confirmPassword: z.string(),
   })
   .refine((schema) => schema.password === schema.confirmPassword, {
